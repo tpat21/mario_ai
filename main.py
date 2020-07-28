3fe7cf1ff8c850f2e73180d6f1e26241e3eee9d0
Tpat21 <41355131+Tpat21@users.noreply.github.com>
2020-07-28 16:55:04 -0400

    Updated main

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os
 import random
 
 WIN_WIDTH = 800
-WIN_HEIGHT = 500
+WIN_HEIGHT = 350
 
-MARIO_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join('images', 'mario.png'))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join('images', 'mario2.png'))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join('images', 'mario3.png')))]
+MARIO_IMGS = [(pygame.image.load(os.path.join('images', 'mario.png'))),
+              (pygame.image.load(os.path.join('images', 'mario2.png'))),
+              (pygame.image.load(os.path.join('images', 'mario3.png')))]
 
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('images', 'pipe.png')))
+PIPE_IMG = (pygame.image.load(os.path.join('images', 'pipe.png')))
 
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('images', 'bg.png')))
 
@@ -25,7 +25,6 @@ class Mario:
     ANIMATION_TIME = 5
 
     def __init__(self, x, y):
-
         self.x = x
         self.y = y
         self.tilt = 0  # degrees to tilt
@@ -36,28 +35,20 @@ class Mario:
         self.img = self.IMGS[0]
 
     def jump(self):
-        """
-        make the bird jump
-        :return: None
-        """
-        self.vel = -10.5
+        self.vel = 0
         self.tick_count = 0
         self.height = self.y
 
-    def move(self):
-        self.tick_count += 1
-
-        # for downward acceleration
-        d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2  # calculate displacement
-
-
-
+    # def move(self):
+    #     self.tick_count += 1
+    #
+    #     d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
 
     def draw(self, win):
 
         self.img_count += 1
 
-        # For animation of bird, loop through three images
+        # For animation of mario, loop through three images
         if self.img_count <= self.ANIMATION_TIME:
             self.img = self.IMGS[0]
         elif self.img_count <= self.ANIMATION_TIME * 2:
@@ -70,7 +61,6 @@ class Mario:
             self.img = self.IMGS[0]
             self.img_count = 0
 
-        # so when bird is nose diving it isn't flapping
         if self.tilt <= -80:
             self.img = self.IMGS[1]
             self.img_count = self.ANIMATION_TIME * 2
@@ -84,14 +74,81 @@ class Mario:
         return pygame.mask.from_surface(self.img)
 
 
-def draw_window(win, bird):
-    win.blit(BG_IMG, (0,0))
-    bird.draw(win)
+class Pipe:
+    VEL = 5
+    HEIGHT = 10
+
+    def __init__(self, x):
+        self.x = x
+        self.location = 0
+        self.PIPE = PIPE_IMG
+        self.passed = False
+        self.bottom = 0
+
+    def set_location(self):
+        self.location = random.randint(50, 450)
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE, (self.x, self.bottom))
+
+    def collide(self, mario):
+        mario_mask = mario.get_mask()
+        pipe_mask = pygame.mask.from_surface(self.PIPE)
+
+        offset = (self.x - mario.x, self.top - round(mario.y))
+
+        point = mario_mask.overlap(pipe_mask, offset)
+
+        if point:
+            return True
+
+        return False
+
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __int__(self, x):
+        self.x = x
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+def draw_window(win, mario, pipes):
+    win.blit(BG_IMG, (0, 0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    # base.draw(win)
+
+    mario.draw(win)
     pygame.display.update()
 
 
 def main():
-    mario = Mario(0, 200 )
+    mario = Mario(0, 200)
+    # base = Base(730)
+    pipes = [Pipe(700)]
+
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -102,9 +159,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        mario.move()
-        draw_window(win, mario)
-
+        # mario.move()
+        draw_window(win, mario, pipes)
 
     pygame.quit()
     quit()
